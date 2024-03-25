8434a1a8cd4fcfe539d2522384f7e66894a2f3ca
ecrudd <signup.git@ecrudd.com>
2024-03-25 09:11:18 -0400

    Addition of gait_analysis.m and trc_design.m
    - Before gait strategy revision (calculates forces for frames identified in Python script as max/min forces -> but this can vary user to user)
    - TRC calculations to be reviewed
    - Gait strategy to be updated by using only thigh angle to determine max/min forces (since this does not change with user or LA length)

--- analysis/system_design.m
+++ analysis/system_design.m
@@ -1,3 +1,8 @@
 
 % material properties: Al alloy 1100 
-alum_1100 = dictionary(elastic_mod, 68900, strength_yield, 24.1, strength_ult, 75.8, density, 2710);
+alum_1100 = dictionary("elastic_mod", 68900, "strength_yield", 24.1, "strength_ult", 75.8, "density", 2710);
+
+
+% Polyethylene - to be changed 
+polyethylene = dictionary("elastic_mod", 860, "strength_yield", 21.9, "strength_ult", 30, "density", 970);
+
